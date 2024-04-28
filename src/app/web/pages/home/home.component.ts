c5231e9623952af7dd039f21c9041a038dd36ab6
Eng. Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2024-04-28 08:52:27 +0300

    feat: got started with the homepage

--- src/app/web/pages/home/home.component.ts
+++ src/app/web/pages/home/home.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const doc = document.getElementById('code');
+    if (!doc) {
+      return;
+    }
+    doc.innerHTML = `
 
+  import { RiskNoteTypeEnum } from '../enums/risk-notes-types.enum';
+
+/**
+ * Return risk note type from the risk note enum
+ *
+ * @export
+ * @param {RiskNoteTypeEnum} type
+ * @return {*}  {string}
+ */
+export function GetRiskNoteNameHelper(type: RiskNoteTypeEnum): string {
+  let riskNoteType = '';
+
+  switch (type) {
+    case RiskNoteTypeEnum.NEW:
+      riskNoteType = 'New Policy Risk Note';
+      break;
+    case RiskNoteTypeEnum.RENEWAL:
+      riskNoteType = 'Renewal Risk Note';
+      break;
+    case RiskNoteTypeEnum.ADDITIONAL_PREMIUM:
+      riskNoteType = 'Additional Premium Endorsement Risk Note';
+      break;
+    case RiskNoteTypeEnum.REFUND:
+      riskNoteType = 'Refund Endorsement Risk Note';
+      break;
+    case RiskNoteTypeEnum.COMESA:
+      riskNoteType = 'Comesa Endorsement Risk Note';
+      break;
+    case RiskNoteTypeEnum.NIL:
+      riskNoteType = 'Nil Endorsement Risk Note';
+      break;
+    case RiskNoteTypeEnum.LAPSE:
+      riskNoteType = 'Lapsed Policy Risk Note';
+      break;
+    default:
+      riskNoteType = 'New Policy Risk Note';
+      break;
+  }
+  return riskNoteType;
+}
+`;
+  }
 }
