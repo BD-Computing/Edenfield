74d2f062b69f24eb56a4616a363b764228ac5398
Eng. Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2024-07-13 00:20:13 +0300

    refactor: add features section

--- src/app/web/pages/home/home.component.ts
+++ src/app/web/pages/home/home.component.ts
@@ -1,58 +1,43 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppName } from 'src/app/constants';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    const doc = document.getElementById('code');
-    if (!doc) {
-      return;
-    }
-    doc.innerHTML = `
-
-  import { RiskNoteTypeEnum } from '../enums/risk-notes-types.enum';
-
-/**
- * Return risk note type from the risk note enum
- *
- * @export
- * @param {RiskNoteTypeEnum} type
- * @return {*}  {string}
- */
-export function GetRiskNoteNameHelper(type: RiskNoteTypeEnum): string {
-  let riskNoteType = '';
-
-  switch (type) {
-    case RiskNoteTypeEnum.NEW:
-      riskNoteType = 'New Policy Risk Note';
-      break;
-    case RiskNoteTypeEnum.RENEWAL:
-      riskNoteType = 'Renewal Risk Note';
-      break;
-    case RiskNoteTypeEnum.ADDITIONAL_PREMIUM:
-      riskNoteType = 'Additional Premium Endorsement Risk Note';
-      break;
-    case RiskNoteTypeEnum.REFUND:
-      riskNoteType = 'Refund Endorsement Risk Note';
-      break;
-    case RiskNoteTypeEnum.COMESA:
-      riskNoteType = 'Comesa Endorsement Risk Note';
-      break;
-    case RiskNoteTypeEnum.NIL:
-      riskNoteType = 'Nil Endorsement Risk Note';
-      break;
-    case RiskNoteTypeEnum.LAPSE:
-      riskNoteType = 'Lapsed Policy Risk Note';
-      break;
-    default:
-      riskNoteType = 'New Policy Risk Note';
-      break;
-  }
-  return riskNoteType;
-}
-`;
-  }
+export class HomeComponent {
+  appName = AppName;
+  features: { icon: string; title: string; description: string }[] = [
+    {
+      icon: 'bi-people',
+      title: 'Customer Management',
+      description: 'Connect with your customers wherever they are',
+    },
+    {
+      icon: 'bi-briefcase-fill',
+      title: 'Underwriting',
+      description: `Your insurance platform wouldn't be complete if it didn't include capabilities to manage policies.`,
+    },
+    {
+      icon: 'bi-file-pdf-fill',
+      title: 'Policy Management',
+      description: `Policy documents are instantly saved at the moment you receive the policy.`,
+    },
+    {
+      icon: 'bi-wallet2',
+      title: 'Premium Collection & Allocation',
+      description: `Collect Premium from the clients, allocate to the policies and remit to the underwriters with confidence and ease`,
+    },
+    {
+      icon: 'bi-piggy-bank',
+      title: 'Commission Request & Allocation',
+      description: `Premium management cannot be complete without commission request. You can request for commission directly in the ERP`,
+    },
+    {
+      icon: 'bi-activity',
+      title: 'Reporting',
+      description: `Access your data to see, report, and improve. As more submissions, quotes, and policies are generated, you need an aggregate view of transactions.`,
+    },
+  ];
 }
